e983d457d542fedc41812866f03e9b1714c76eaf
SysWhite <76256827+SysWhiteDev@users.noreply.github.com>
2022-05-02 15:36:04 +0200

    Add files via upload

---
+++ pages/minigames/osu/src/scripts/version.js
@@ -0,0 +1,7 @@
+const BUILD_PREFIX = "osu(browser)!";
+const MAJOR_VERSION = 0;
+const MINOR_VERSION = 0;
+const PATCH_VERSION = 1;
+const BUILD_METADATA = "beta";
+const Version = `${BUILD_PREFIX}${MAJOR_VERSION}.${MINOR_VERSION}.${PATCH_VERSION}${BUILD_METADATA}`;
+export {Version};
