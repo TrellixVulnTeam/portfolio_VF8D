30eaf16b38b46683fa6cb25c7737e6607dbdaac7
SysWhite <micsimfrayt@gmail.com>
2022-08-27 15:03:47 +0200

    fixmobile

--- mobile.js
+++ mobile.js
@@ -2,7 +2,6 @@ const doc = document;
 const menuOpen = doc.querySelector(".menu");
 const menuClose = doc.querySelector(".close");
 const overlay = doc.querySelector(".overlay");
-
 menuOpen.addEventListener("click", () => {
   overlay.classList.add("overlay--active");
 });
